6d6c8ac7de5fe921275f15cdb396bf0b88eb0f54
swadeepmohanty <swadeepmohanty@gmail.com>
2019-04-27 11:02:00 +0530

    first commit

---
+++ src/main/java/com/learnreactivestream/LearnReactivestreamApplication.java
@@ -0,0 +1,13 @@
+package com.learnreactivestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactivestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactivestreamApplication.class, args);
+	}
+
+}
